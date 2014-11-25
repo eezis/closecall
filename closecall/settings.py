033eb8307ccd7b79816d4cec1821bd42640aa103
Ernest Ezis <ernest.ezis@gmail.com>
2014-11-25 15:50:20 -0700

    nginx.conf the hunt for service static files changing the static stuff in settings.py to match the amlit stuff

--- closecall/settings.py
+++ closecall/settings.py
@@ -102,10 +102,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = 'http://closecalldatabase.com/static/'
 
 # https://docs.djangoproject.com/en/1.7/howto/static-files/#deployment
-STATIC_ROOT = '/home/eezis/sites/static/closecall/'
+# STATIC_ROOT = '/home/eezis/sites/static/closecall/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
 
 TEMPLATE_DIRS = (
