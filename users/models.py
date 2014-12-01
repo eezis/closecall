ed44c5b070938b0f913426869146078944545dee
Ernest Ezis <ernest.ezis@gmail.com>
2014-11-30 17:16:07 -0700

    updates

--- users/models.py
+++ users/models.py
@@ -9,9 +9,9 @@ class UserProfile(BaseFields):
     city = models.CharField(null=True, max_length=120)
     state = models.CharField(null=True, blank=True, max_length=50)
     country = models.CharField(null=True, max_length=80)
-    zipcode = models.CharField(null=True, blank=True, max_length=30)
+    zipcode = models.CharField(null=True, blank=True, max_length=30, verbose_name="Zip/Postal Code")
     # does the user want email notifications when new incidents are reported in their area?
-    email_incidents = models.BooleanField(default=True, verbose_name="Send Email When Incidents Occur In Your Area")
+    email_incidents = models.BooleanField(default=True, verbose_name="Email Me When Incidents Are Reported In My Area")
     # sms_incidents = models.BooleanField(deault=True, verbose_name="Receive")
     # sms would require phone numbers
 
