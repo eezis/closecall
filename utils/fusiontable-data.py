4ff1d67a04dc93dc93ca3f4b3e5260089f8c5bd8
Ernest Ezis <ernest.ezis@gmail.com>
2014-12-26 13:59:33 -0700

    Registration map to home page

---
+++ utils/fusiontable-data.py
@@ -0,0 +1,29 @@
+"""
+Gets the data to paste into data/test.csv that then gets imported into the fusiontable on googledrive
+"""
+
+import sys
+try:
+    sys.path.append("/Users/eae/code/sites/closecall")
+    sys.path.append("/Users/eae/.virtualenvs/closecall/lib/python2.7/site-packages")
+except ImportError:
+    print "import error -- probably on production machine -- trying that . . ."
+    try:
+        sys.path.append("/home/eezis/sites/closecall")
+        sys.path.append("/home/eezis/.virtualenvs/closecall/bin/python2.7/site-packages")
+        print "import complete"
+    except:
+        "Can't import that virtualenv, is the path correct?"
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'closecall.settings'
+import django
+django.setup()
+
+from users.models import UserProfile
+
+UPS = UserProfile.objects.all()
+
+for u in UPS:
+    print u'"{}, {}, {}, {}", {}'.format(u.city, u.state, u.country, u.zipcode, u.created)
+
