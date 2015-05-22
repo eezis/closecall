a78b083651887c1bd7bebeb481639368c6514986
Ernest Ezis <ernest.ezis@gmail.com>
2015-05-22 13:27:25 -0600

    Adding pic for incident 179

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -25,7 +25,7 @@ from core.utils import distance_between_geocoded_points
 from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
-INCIDENT_ID = 179
+INCIDENT_ID = 185
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 TESTING = True
 
