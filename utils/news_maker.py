150d308623c4ff0d126c4bf1da40bec8800a4f9a
Ernest Ezis <ernest.ezis@gmail.com>
2015-08-11 14:16:19 -0600

    pic for 273

---
+++ utils/news_maker.py
@@ -0,0 +1,38 @@
+
+
+
+story_url = "http://www.burlingtonfreepress.com/story/news/local/2015/08/03/police-blame-cyclist-fatal-crash/31088263/"
+
+news_source = "Burlington Free Press"
+
+my_title = "Driver Mixing Alcohol and Meds Kills Cyclist. Police Blame Cyclist."
+
+hat_tip = ""
+
+comments = """
+I cannot say this enough: if you are a cyclist, get an on-bike camera. Do it for your family.
+
+This is not a pleasant story. Unfortunately, it's not a common one. Driver strikes and kills cyclist. As the only remaining witness, the driver's version of the "truth" becomes The Truth.
+
+As every rider knows, and as the law clearly provides, there are times when a cyclist -- for their own safety -- must abandon the principle “that the rider shall ride as near to the right side of the roadway as practicable.” In this case, we have two drivers and one cyclist. The first driver is sober. The second driver is drunk and on three different medications; Xanax, Sertraline and Nortriptyline.
+
+For the record, here are warnings associated with each of the drugs:
+
+Nortriptyline: Do not drink alcohol. Nortriptyline can increase the effects of alcohol, which could be dangerous.
+
+Xanax: Do not drink alcohol while taking Xanax. This medication can increase the effects of alcohol.
+
+Sertraline: Drinking alcohol can increase certain side effects of sertraline (side effects include drowsiness and dizziness)
+
+So our baseline is established. One sober driver, one drunk driver that is probably experiencing increased of drunkeness, and one cyclist, Dr.Kenneth Najarian. All three people were headed southbound on Greenbush Road in Ferrisburgh, VT. The sober driver passed the cyclist without incident. The drunk and medicated driver struck and killed the cylist.
+
+So what happens next? Something that happens with alarming frequency, something so common that it seems to be the exception rather than the rule. The police blame the cyclist.
+
+While the cyclist is held to the strictest interpretation of the law -- one that even provides for exceptions -- the driver who is driving drunk and impaired is only charged with drunk driving rather than the more severe charge of DUI with death resulting. Which is odd to me, considering she was Driving Under the Influence and a death resulted.
+
+
+"Police say the driver of the car was Holly Gonyeau, 36, of Ferrisburgh. And police say she was driving drunk. A preliminary breath test shows her blood alcohol content was 0.123. Gonyeau was cited into court for drunk driving, but not the more severe charge of DUI with death resulting."
+
+So, I want to scream this at the top of my lungs to every cyclist in the world. Buy a #$(#&#) video camera for your bike and use it! If something happens to you it may be the only thing that provides objective evidence of what actually happened and who was at fault.
+
+"""
