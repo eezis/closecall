20b4df8e98b5985747661e14f049c79d3d7a1c03
Ernest Ezis <ernest.ezis@gmail.com>
2015-11-18 15:37:36 -0700

    fixed search field in admin

--- incident/admin.py
+++ incident/admin.py
@@ -5,7 +5,7 @@ from incident.models import Incident
 # Register your models here.
 
 class IncidentAdmin(admin.ModelAdmin):
-    search_fields = ['user',]
+    search_fields = ['user', 'what']
     # list_filter = ('user',) <-- this makes an exhaustive list with every user, I so make that a search rather than filter
     ordering = ('-created',)
 
