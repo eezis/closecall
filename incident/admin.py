3f4c5ac92bc5f465e797715ab16b88d2d69f90f2
Ernest Ezis <ernest.ezis@gmail.com>
2015-11-19 12:24:25 -0700

    Search incidents by last name or email

--- incident/admin.py
+++ incident/admin.py
@@ -5,7 +5,7 @@ from incident.models import Incident
 # Register your models here.
 
 class IncidentAdmin(admin.ModelAdmin):
-    search_fields = ['user', 'what']
+    search_fields = ['user__last_name', 'user__email', 'what',]
     # list_filter = ('user',) <-- this makes an exhaustive list with every user, I so make that a search rather than filter
     ordering = ('-created',)
 
