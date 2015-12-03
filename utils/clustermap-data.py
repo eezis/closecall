00b9e4ab036b86ede2baee10ea158ef3ace2e309
Ernest Ezis <ernest.ezis@gmail.com>
2015-12-02 19:40:17 -0700

    update clutermap data script to work at server

--- utils/clustermap-data.py
+++ utils/clustermap-data.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 
+"""
+produce a file in this format, where count = number of records.
+
+var data = { "count": 5300,
+  "members": [
+  {"member": 3, "longitude": -105.2838511, "latitude": 40.0454736 },
+  {"member": 8, "longitude": -105.2838511, "latitude": 40.0454736 },
+  {"member": 10, "longitude": -105.2838511, "latitude": 40.0454736 },
+  {"member": 12, "longitude": -105.1019275, "latitude": 40.1672068 },
+  {"member": 13, "longitude": -105.2077798, "latitude": 40.0005378 },
+  {"member": 14, "longitude": -105.2838511, "latitude": 40.0454736 },
+  {"member": 15, "longitude": -105.2399774, "latitude": 40.0293099 },
+  {"member": 5454, "longitude": -118.542586, "latitude": 34.3916641 },
+  {"member": 5455, "longitude": -77.1398878, "latitude": 38.8485316 }
+ ]}
+
+
+"""
+
+
 import sys
 reload(sys) # just to be sure
 sys.setdefaultencoding('utf-8')
 import os
+import shutil
 
 # get the OS indendent home direction
 home_dir = os.path.expanduser("~")
@@ -17,52 +38,112 @@ django.setup()
 
 from users.models import UserProfile
 
+from django.conf import settings
 
-# doesn't need to be ordered, but useful during development
-# UPS = UserProfile.objects.all()
-UPS = UserProfile.objects.all().order_by('id')
+new_data_file = "{}/nginx-root/new-data.json".format(settings.PROJECT_ROOT)
+data_file = "{}/nginx-root/data.json".format(settings.PROJECT_ROOT)
 
-print UPS.count()
 
-total = UPS.count()
-# total = 50
+def create_the_file():
+    print 'creating the new file'
+    UPS = UserProfile.objects.all().order_by('id')
+    fh = open(new_data_file, 'w')
 
-print 'var data = {{ "count": {},'.format(total)
-print '  "members": ['
-count = 0
-# for u in UPS[:total]:
-for u in UPS:
-    count += 1
-    # SO THIS CALL WILL TRIGGER SOME PRINT STATEMENTS IF A NEW POSITION NEEDS TO BE GEOCODED
-    # THAT'S A PROBLEM?
-    # PROBABLY NOT IF BUILDING A STRING AND WRITING TO A FILE -- like I was testing in ipython
-    lat, lon = u.get_lat_lon()
-    # print '  {{"{}": {}, "longitude": {}, "latitude": {} }}'.format('member', 'test', lat, lon)
+    total = UPS.count()
+
+    fh.write('var data = {{ "count": {},\n'.format(total))
+    fh.write('  "members": [\n')
+    count = 0
+    for u in UPS:
+        count += 1
+        # SO THIS CALL WILL TRIGGER SOME PRINT STATEMENTS IF A NEW POSITION NEEDS TO BE GEOCODED
+        # THAT'S A PROBLEM?
+        # PROBABLY NOT IF BUILDING A STRING AND WRITING TO A FILE -- like I was testing in ipython
+        lat, lon = u.get_lat_lon()
+        # print '  {{"{}": {}, "longitude": {}, "latitude": {} }}'.format('member', 'test', lat, lon)
+        if count < total:
+            fh.write('  {{"{}": {}, "longitude": {}, "latitude": {} }},\n'.format('member', u.id, lon, lat))
+        else:
+            fh.write('  {{"{}": {}, "longitude": {}, "latitude": {} }}\n'.format('member', u.id, lon, lat))
+
+
+    fh.write( ' ]}\n')
+
+
+
+def copy_the_file():
+    print 'copying the file'
+    src = new_data_file
+    dst = data_file
+    shutil.copy(src, dst)
+
+
+
+
+def update_data_file():
+    create_the_file()
+    copy_the_file()
+    print 'done'
+
+update_data_file()
+
+
+"""
+to view it in the console
+"""
+
+if 0 == 1:
+
+    # doesn't need to be ordered, but useful during development
+    # UPS = UserProfile.objects.all()
+    UPS = UserProfile.objects.all().order_by('id')
+
+    print UPS.count()
+
+    total = UPS.count()
+    # total = 50
+
+    print 'var data = {{ "count": {},'.format(total)
+    print '  "members": ['
+    count = 0
+    # for u in UPS[:total]:
+    for u in UPS:
+        count += 1
+        # SO THIS CALL WILL TRIGGER SOME PRINT STATEMENTS IF A NEW POSITION NEEDS TO BE GEOCODED
+        # THAT'S A PROBLEM?
+        # PROBABLY NOT IF BUILDING A STRING AND WRITING TO A FILE -- like I was testing in ipython
+        lat, lon = u.get_lat_lon()
+        # print '  {{"{}": {}, "longitude": {}, "latitude": {} }}'.format('member', 'test', lat, lon)
+        if count < total:
+            print '  {{"{}": {}, "longitude": {}, "latitude": {} }},'.format('member', u.id, lon, lat)
+        else:
+            print '  {{"{}": {}, "longitude": {}, "latitude": {} }}'.format('member', u.id, lon, lat)
+
+
+    print ' ]}'
+
+
+
+
+
+
+
+def make_big_string(UPS):
+    count = 0
+    jdata = 'var data = {{ "count": {},\n'.format(UPS.count)
+    jdata = jdata + '  "members": ['
+    for u in UPS:
+        count += 1
+        lat, lon = u.get_lat_lon()
+
     if count < total:
         print '  {{"{}": {}, "longitude": {}, "latitude": {} }},'.format('member', u.id, lon, lat)
     else:
         print '  {{"{}": {}, "longitude": {}, "latitude": {} }}'.format('member', u.id, lon, lat)
 
 
-print ' ]}'
 
-
-
-# def make_big_string(UPS):
-#     count = 0
-#     jdata = 'var data = {{ "count": {},\n'.format(UPS.count)
-#     jdata = jdata + '  "members": ['
-#     for u in UPS:
-#         count += 1
-#         lat, lon = u.get_lat_lon()
-
-#     if count < total:
-#         print '  {{"{}": {}, "longitude": {}, "latitude": {} }},'.format('member', u.id, lon, lat)
-#     else:
-#         print '  {{"{}": {}, "longitude": {}, "latitude": {} }}'.format('member', u.id, lon, lat)
-
-
-# print ' ]}'
+    print ' ]}'
 
 
 # https://googlemaps.github.io/js-marker-clusterer/examples/data.json
