d24254446deebb5ab10f1e874439172101cf033f
Ernest Ezis <ernest.ezis@gmail.com>
2015-12-03 18:20:48 -0700

    hotfix

--- incident/views.py
+++ incident/views.py
@@ -183,12 +183,12 @@ def show_this_incident(request, ee_fake_key):
 def show_this_incident_for_authed_users(request, incident_id):
     #the key must be 8 characters long, [a-z0-9-] ee-1-173
 
-    if request.user.is_authenticated():
-        username = request.user.username
-        useremail = request.user.email
-        expires_in_x_seconds = request.session.get_expiry_date()
-        print
-        print "FOR USER {}, AT EMAIL {}, THE SESSION WILL EXPIRE ON: {}".format(expires_in_x_seconds)
+    # if request.user.is_authenticated():
+    #     username = request.user.username
+    #     useremail = request.user.email
+    #     expires_in_x_seconds = request.session.get_expiry_date()
+    #     print
+    #     print "FOR USER {}, AT EMAIL {}, THE SESSION WILL EXPIRE ON: {}".format(expires_in_x_seconds)
 
     I = Incident.objects.get(id=incident_id)
     if I.visible:
