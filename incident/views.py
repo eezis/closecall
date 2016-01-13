3a11605271b3abeb2f04fbc3d960a0358c7f7498
Ernest Ezis <ernest.ezis@gmail.com>
2016-01-13 12:19:46 -0700

    hot fix

--- incident/views.py
+++ incident/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
