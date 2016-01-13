d81be77ac845461755bf1b2fc2fcaa0b0c2c40b2
Ernest Ezis <ernest.ezis@gmail.com>
2016-01-13 12:13:56 -0700

    hot fix

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -26,14 +26,14 @@ from core.utils import distance_between_geocoded_points
 from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
-INCIDENT_ID = 407
+INCIDENT_ID = 418
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
 TESTING = True
 MAIL_TO_EE = False
 
-Radius = 10
-# Radius = 30
+#Radius = 10
+Radius = 30
 # Radius = 40
 # Radius = 60
 
