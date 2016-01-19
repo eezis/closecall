6ef977a8753a579f0eb48e851ab54275c1caf148
Ernest Ezis <ernest.ezis@gmail.com>
2016-01-19 08:58:23 -0700

    Pics for 421

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -26,16 +26,16 @@ from core.utils import distance_between_geocoded_points
 from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
-INCIDENT_ID = 418
+INCIDENT_ID = 420
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
 TESTING = True
 MAIL_TO_EE = False
 
 #Radius = 10
-Radius = 30
+# Radius = 30
 # Radius = 40
-# Radius = 60
+Radius = 60
 
 subject = "Close Call Database - Incident Reported in your Area"
 
