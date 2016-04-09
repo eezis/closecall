15066b154da5feeb1031d12931b29c50307e3130
Ernest Ezis <ernest.ezis@gmail.com>
2016-04-09 08:16:14 -0600

    Pic for 483

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -34,10 +34,10 @@ from core.utils import distance_between_geocoded_points
 from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
-INCIDENT_ID = 477
+INCIDENT_ID = 486
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
-TESTING = True
+TESTING = False
 MAIL_TO_EE = False
 
 # Radius = 10
