4febc33395c44c4287e4a351359376c0c3764c6c
Ernest Ezis <ernest.ezis@gmail.com>
2016-06-29 09:20:59 -0700

    Add image for 596

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -35,7 +35,7 @@ from core.utils import distance_between_geocoded_points
 from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
-INCIDENT_ID = 570
+INCIDENT_ID = 588
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
 TESTING = True
