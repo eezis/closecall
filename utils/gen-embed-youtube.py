2b799a20466ca1ee15f58198697040a7cb672dab
Ernest Ezis <ernest.ezis@gmail.com>
2016-08-12 16:15:31 -0600

    Adding pics for reports

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/W2jk15yhAIQ'
+youtube_url = 'https://youtu.be/mY_zeEMJYqA'
 
 
 def create_html_for_youtube(video_url):
