751a60b84d376b04f15d2be5a780b1a1218f2337
Ernest Ezis <ernest.ezis@gmail.com>
2016-08-31 07:29:55 -0600

    Pic for 702

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -36,7 +36,7 @@ from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
 
-INCIDENT_ID = 704
+INCIDENT_ID = 706
 
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
