96eb2932bd998208c985497842991564ca9036bb
Ernest Ezis <ernest.ezis@gmail.com>
2016-09-02 08:53:39 -0600

    Adding pic for 719

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://www.facebook.com/motorcyclemagician/videos/10208139685744692/'
+youtube_url = 'https://youtu.be/M0uUz_dSbsg'
 
 
 def create_html_for_youtube(video_url):
