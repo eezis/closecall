583d85f92268bfbfeac74b8800ef71d56f1f98de
Ernest Ezis <ernest.ezis@gmail.com>
2016-09-07 16:58:57 -0600

    pics for 725 and 731

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/M0uUz_dSbsg'
+youtube_url = 'https://youtu.be/4SaNXRDADfE'
 
 
 def create_html_for_youtube(video_url):
