32d4863567f0bc10b1cf9160e35ccd5b10cb0f6d
Ernest Ezis <ernest.ezis@gmail.com>
2016-11-02 11:54:21 -0600

    pics

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -36,7 +36,7 @@ from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
 
-INCIDENT_ID = 903
+INCIDENT_ID = 9xx
 
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
@@ -44,9 +44,9 @@ TESTING = True
 MAIL_TO_EE = False
 
 # Radius = 10
-# Radius = 15
+Radius = 15
 # Radius = 20
-Radius = 30
+# Radius = 30
 # Radius = 40
 # Radius = 50
 # Radius = 60
