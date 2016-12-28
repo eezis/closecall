a48a4f5130259ed3bbcce82cdfa2661a849880be
Ernest Ezis <ernest.ezis@gmail.com>
2016-12-27 17:27:28 -0700

    pics

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/CUhNa-NkGOg'
+youtube_url = 'https://youtu.be/wsoaO8J4Yss'
 
 
 def create_html_for_youtube(video_url):
