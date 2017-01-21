f1935ea79aa46c5e4c22ffbc76f87bb342e34a24
Ernest Ezis <ernest.ezis@gmail.com>
2017-01-21 08:37:12 -0700

    adding SSL via letsencrypt

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -36,7 +36,8 @@ from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
 
-INCIDENT_ID = 1046
+INCIDENT_ID = 1058
+
 
 
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
@@ -46,9 +47,9 @@ MAIL_TO_EE = False
 
 # Radius = 10
 # Radius = 15
-# Radius = 20
+Radius = 20
 # Radius = 30
-Radius = 40
+# Radius = 40
 # Radius = 50
 # Radius = 60
 
