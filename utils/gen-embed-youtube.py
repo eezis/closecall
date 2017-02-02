075488c16434b4305cc11aa45ceb5f349fa251d2
Ernest Ezis <ernest.ezis@gmail.com>
2017-02-02 13:30:22 -0700

    http -> https for maps.google.com

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/P3mG24OMNeM'
+youtube_url = 'https://youtu.be/KaeBQqLYIWQ'
 
 
 def create_html_for_youtube(video_url):
