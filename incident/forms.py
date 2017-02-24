cbdfc12f8c72b5873ef8996ff922caa966695671
Ernest Ezis <ernest.ezis@gmail.com>
2017-02-23 21:11:53 -0700

    moved the long string to the forms.py

--- incident/forms.py
+++ incident/forms.py
@@ -7,7 +7,31 @@ from django.forms.extras.widgets import SelectDateWidget
 # from tinymce.widgets import TinyMCE
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
-from models import Incident, what_verbose_str
+from models import Incident
+
+what_verbose_str = """
+Now describe what happened. Be factual, include direction of travel for cyclists and vehicles. Example: <p style="font-size:0.90em;margin-top:10px;margin-left:24px;
+    margin-right:30px;">
+
+    I was traveling southbound on Westminster Road, two other cyclists were riding immediately behind me. The driver of a white pickup truck, also traveling south,
+    started to honk his horn at us . . . your specifics details . . . </p>
+    <p style="font-size:0.90em;margin-top:10px;margin-left:24px;margin-right:30px;">There was very little traffic on the road at the time of the encounter. The lighting was good,
+    all cyclists were inside the bike lane. </p>
+
+    <p style="font-size:0.90em;margin-top:10px;margin-left:24px;margin-right:30px;"><i>
+    <span style="color:red">If you know the identity and home address of the driver, please do not include that information
+    in this report. You can email me that information (closecalldatabase@gmail.com) and I will include it in the non-public notes.</span></i>
+    </p>
+
+    <p style="font-size:0.90em;margin-top:10px;margin-left:24px;margin-right:30px;"><i>
+    If you have <span style="color:red">VIDEO</span> that you have posted to youtube, vimeo or a similar location, please email the URL to me. If you have a <span style="color:red">PICTURE</span> or two to accompany your report please email those as well closecalldatabase@gmail.
+    I will embed them in the report.
+    </i></p>
+
+
+Tell your story with enough context so that it can be understood by cyclists that were not there and may be unfamiliar with the location.
+"""
+
 
 
 
