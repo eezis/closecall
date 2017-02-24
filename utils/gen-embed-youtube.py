a7300d30041a086f7cf0ccc1bc7a6df44d1752bd
Ernest Ezis <ernest.ezis@gmail.com>
2017-02-23 21:02:03 -0700

    routine

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/XoNrNUa8Dx0'
+youtube_url = 'https://youtu.be/ssaPa9e-d2A'
 
 
 def create_html_for_youtube(video_url):
