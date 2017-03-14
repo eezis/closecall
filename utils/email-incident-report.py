9074a31e01b520cd6c2bd788fb7e9a9211a016ac
Ernest Ezis <ernest.ezis@gmail.com>
2017-03-14 11:29:46 -0600

    pics

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -36,7 +36,7 @@ from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
 
-INCIDENT_ID = 1143
+INCIDENT_ID = 1155
 
 
 
@@ -48,10 +48,10 @@ MAIL_TO_EE = False
 # Radius = 10
 # Radius = 15
 # Radius = 20
-# Radius = 30
+Radius = 30
 # Radius = 40
 # Radius = 50
-Radius = 60
+# Radius = 60
 
 subject = "Close Call Database - Incident Reported in your Area"
 
--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/eU-d0LwX53k'
+youtube_url = 'https://youtu.be/dLRTm-IboR0'
 
 
 def create_html_for_youtube(video_url):
