d2b63f5aa89e8fddfb12d67c0f045a3c3f1c5431
Ernest Ezis <ernest.ezis@gmail.com>
2017-03-25 18:20:24 -0400

    Allowed Hosts

--- closecall/settings.py
+++ closecall/settings.py
@@ -32,7 +32,8 @@ TEMPLATE_DEBUG = True
 
 # ALLOWED_HOSTS = ['*', '104.131.56.181', '.closecalldatabase.com', '127.0.0.1', 'localhost', 'closecall', ]
 # ALLOWED_HOSTS = ['*', ]
-ALLOWED_HOSTS = ['.closecalldatabase.com', '127.0.0.1', ]
+# ALLOWED_HOSTS = ['.closecalldatabase.com', '127.0.0.1', ]
+ALLOWED_HOSTS = ['.closecalldatabase.com', ]
 
 # this is set by the second so 60 * 5 = 5 minutes
 # fifteen months - I went from 3 months to 15 on 12/3/15
