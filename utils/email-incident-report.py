9b857e27da8fed4125754e7843acc9f025e943e5
Ernest Ezis <ernest.ezis@gmail.com>
2017-04-24 20:14:00 -0600

    Adding pics

--- utils/email-incident-report.py
+++ utils/email-incident-report.py
@@ -36,13 +36,13 @@ from core.views import send_incident_notification
 from django.contrib.auth.models import User
 
 
-INCIDENT_ID = 1222
+INCIDENT_ID = 1226
 
 
 
 # TWEAK THE INCIDENT_ID CONSTANT UP TOP!
 
-TESTING = True
+TESTING = False
 MAIL_TO_EE = False
 
 # Radius = 10
@@ -50,8 +50,8 @@ MAIL_TO_EE = False
 # Radius = 25
 # Radius = 30
 # Radius = 40
-Radius = 50
-# Radius = 60
+# Radius = 50
+Radius = 60
 
 
 subject = "Close Call Database - Incident Reported in your Area"
