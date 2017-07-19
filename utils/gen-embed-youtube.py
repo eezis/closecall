a612a8325226a90ff6cb4975b73ea4732a62eae2
Ernest Ezis <ernest.ezis@gmail.com>
2017-07-18 19:18:25 -0600

    clarified language

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/_u1M7oq4h8o'
+youtube_url = 'https://youtu.be/tqhY8BcaOKA'
 
 
 def create_html_for_youtube(video_url):
