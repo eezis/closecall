2c65ff22f9551a52904ce5a4f7062617f8f1e449
Ernest Ezis <ernest.ezis@gmail.com>
2017-09-05 11:13:41 -0600

    Adding pics

--- utils/gen-embed-youtube.py
+++ utils/gen-embed-youtube.py
@@ -2,7 +2,7 @@
 # <iframe width="560" height="315" src="//www.youtube.com/embed/tVAvp9a82TM" frameborder="0" allowfullscreen></iframe>
 
 
-youtube_url = 'https://youtu.be/tqhY8BcaOKA'
+youtube_url = 'https://youtu.be/QBpEKP1VuYQ'
 
 
 
